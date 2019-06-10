a6bd7101b95e1894aa6afbf057b4aa67db7bc666
kangdwfood2827 <kangdw.food2827@gmail.com>
2019-06-11 01:28:27 +0900

    Updates

---
+++ precache-manifest.232b60b43de36fc9acdb059a1efd1518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64321ccf805261d6d6276a686f727bbe",
+    "url": "/kangdw.food2827/index.html"
+  },
+  {
+    "revision": "f0510234a3672794426b",
+    "url": "/kangdw.food2827/static/css/main.e9c2d307.chunk.css"
+  },
+  {
+    "revision": "7ee996b186ee25925c1f",
+    "url": "/kangdw.food2827/static/js/2.f0acff67.chunk.js"
+  },
+  {
+    "revision": "f0510234a3672794426b",
+    "url": "/kangdw.food2827/static/js/main.ffc51378.chunk.js"
+  },
+  {
+    "revision": "e062ad0a644ab41177f0",
+    "url": "/kangdw.food2827/static/js/runtime~main.46fd9aa3.js"
+  },
+  {
+    "revision": "e83d83f34c3b3bc6f7dfa3e542f2b4f2",
+    "url": "/kangdw.food2827/static/media/food.e83d83f3.png"
+  }
+]);
